ec5b99d9c8dc4df6a89f34fcacd5ffee33eb2e21
Mark Fowler <themindbadger@gmail.com>
2017-02-26 21:06:54 +0000

    Added some more useful SQL queries

--- src/main/resources/useful_queries.sql
+++ src/main/resources/useful_queries.sql
@@ -10,6 +10,23 @@ and		d.div_id = sd.div_id
 group by sd.ssn_num, d.div_name
 order by sd.ssn_num desc, sd.div_pos;
 
+-- Find how many fixtures don't have a date
+select ssn_num, count(*)
+from fixture where fixture_date is null
+group by ssn_num
+order by ssn_num;
+
+select *
+from fixture where fixture_date is null;
+
+
+-- Find how many fixtures prior to today that have a fixture date, but no score
+select *
+from fixture where fixture_date is not null
+and home_goals is null
+and fixture_date <= curdate()
+order by ssn_num;
+
 -- Find fixtures that should exist, but don't
 select	sd_h.ssn_num, d.div_name, ht.team_name, awt.team_name, count(*)
 from 	season_division sd_h,
@@ -46,7 +63,8 @@ and		f.ssn_num = sdt.ssn_num
 and		f.div_id = sdt.div_id
 and		(f.home_team_id = sdt.team_id or f.away_team_id = sdt.team_id)
 group by sdt.ssn_num, sdt.div_id, sdt.team_id
-order by sdt.ssn_num desc, sdt.div_id, sdt.team_id;
+order by sdt.ssn_num desc, sdt.div_id, sdt.team_id
+;
 
 
 
@@ -66,7 +84,7 @@ and		f.ssn_num = 2016
 order by	f.fixture_date, sd.div_pos, ht.team_name;
 
 -- Retrieve all fixtures for a season that do not have a fixture date
-select	f.fixture_id, d.div_name, ht.team_name, at.team_name, f.fixture_date, f.home_goals, f.away_goals
+select	f.fixture_id, f.ssn_num, d.div_name, ht.team_id, ht.team_name, at.team_id, at.team_name, f.fixture_date, f.home_goals, f.away_goals
 from	fixture f,
 		division d,
         team ht,
@@ -77,9 +95,8 @@ and		ht.team_id = f.home_team_id
 and		at.team_id = f.away_team_id
 and		sd.ssn_num = f.ssn_num
 and		sd.div_id = f.div_id
-and		f.ssn_num = 2016
 and		f.fixture_date is null
-order by	f.fixture_date, sd.div_pos, ht.team_name;
+order by	f.ssn_num desc, sd.div_pos, ht.team_name;
 
 -- Retrieve all unplayed fixtures prior to today that have a fixture date, but no score
 select	f.ssn_num, f.fixture_id, d.div_name, ht.team_name, at.team_name, f.fixture_date, f.home_goals, f.away_goals
@@ -97,3 +114,16 @@ and		f.fixture_date is not null
 and		f.fixture_date < curdate()
 and		f.home_goals is null
 order by	f.fixture_date, sd.div_pos, ht.team_name;
+
+-- Get the teams in each division in a season
+select	sd.ssn_num, d.div_name, t.team_id, t.team_name
+from	division d,
+        team t,
+        season_division sd,
+        season_division_team sdt
+where	sd.ssn_num = 2002
+and		sdt.ssn_num = sd.ssn_num
+and		sdt.div_id = sd.div_id
+and		d.div_id = sd.div_id
+and		t.team_id = sdt.team_id
+order by	sd.ssn_num, sd.div_pos, t.team_name;
