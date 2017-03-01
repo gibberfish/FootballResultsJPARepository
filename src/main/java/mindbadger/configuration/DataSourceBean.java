f8a8abc72f75d13db90de66f08e64c4f4274d503
Mark Fowler <themindbadger@gmail.com>
2017-03-01 11:55:23 +0000

    Moved db configuration from properties to Java

---
+++ src/main/java/mindbadger/configuration/DataSourceBean.java
@@ -0,0 +1,27 @@
+package mindbadger.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class DataSourceBean {
+
+	@ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    @Primary
+	public DataSource getDataSource() {
+		return DataSourceBuilder.create()
+			.url("jdbc:mysql://localhost/football_test")
+			.username("root")
+			.password("password")
+			.driverClassName("com.mysql.jdbc.Driver")
+			.build();
+	}
+}
